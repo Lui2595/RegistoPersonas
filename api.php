7a8e5e1b1158724512fc4448a5f19199310b4768
LuisRivka <103442154+LuisRivka@users.noreply.github.com>
2024-03-21 13:25:40 -0600

    update

--- api.php
+++ api.php
@@ -61,5 +61,94 @@
                 # code...
                 break;
         }
+
+        switch ($_POST["action"]) {
+            case 'get':
+                // Example query
+                
+                $sql = "SELECT * FROM detectados ";
+
+                // Execute query
+                $result = $conn->query($sql);
+
+                if ($result === false) {
+                    // Handle query error
+                    echo "Error executing query: " . $conn->error;
+                } else {
+                    // Check if any rows were returned
+                    if ($result->num_rows > 0) {
+                        // Output data of each row
+                        $array = [];
+                        while ($row = $result->fetch_assoc()) {
+                            // Do something with the data
+                            $row["objeto"] = json_decode($row["objeto"],false);
+                            $array[] = $row;
+                        }
+                        $response["data"] = $array;
+                        $response["status"] = 200;
+                        $response["message"] = "Data";
+                        echo json_encode($response);
+                        exit;
+                    } 
+                    echo json_encode($response);
+                    exit;
+                }
+                $conn->close();
+                break;
+            case 'insert':
+                // Example query
+                $json = json_encode($_POST["objeto"]);
+                $sql = "INSERT INTO detectados VALUES (null, $json)";
+
+                // Execute query
+                $result = $conn->query($sql);
+                $conn->close();
+                $response["status"] = 200;
+                $response["message"] = "insertado con esxito";
+                echo json_encode($response);
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+
+    }
+
+    if(isset($_GET["action"])) {
+        switch ($_GET["action"]) {
+            case 'get':
+                // Example query
+                
+                $sql = "SELECT * FROM detectados ";
+
+                // Execute query
+                $result = $conn->query($sql);
+
+                if ($result === false) {
+                    // Handle query error
+                    echo "Error executing query: " . $conn->error;
+                } else {
+                    // Check if any rows were returned
+                    if ($result->num_rows > 0) {
+                        // Output data of each row
+                        $array = [];
+                        while ($row = $result->fetch_assoc()) {
+                            // Do something with the data
+                            $row["objeto"] = json_decode($row["objeto"],false);
+                            $array[] = $row;
+                        }
+                        $response["data"] = $array;
+                        $response["status"] = 200;
+                        $response["message"] = "Data";
+                        echo json_encode($response);
+                        exit;
+                    } 
+                    echo json_encode($response);
+                    exit;
+                }
+                $conn->close();
+                break;
+        }
     }
 ?>
